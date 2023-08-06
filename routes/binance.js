b8d9a97b4cda5b8ba025ed9c5506c6870bf7eec2
awan <aoneholics@gmail.com>
2023-08-06 17:06:31 +0700

    route MA

--- routes/binance.js
+++ routes/binance.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const BinanceController = require('../controllers/binance');
 const BinanceArifController = require('../controllers/binancearif');
+const BinanceMAController = require('../controllers/binancema');
 
 const router = express.Router();
 router.get('/', (req, res)=>{
@@ -8,6 +9,8 @@ router.get('/', (req, res)=>{
         message: "Welcome!"
     })
 });
+
+//AWAN
 router.get('/servertime', BinanceController.getServertime);
 router.get('/balance', BinanceController.getBalance);
 router.get('/position', BinanceController.getPosition);
@@ -15,11 +18,20 @@ router.get('/tradelist', BinanceController.getTradelist);
 router.post('/posttrade', BinanceController.postTrade);
 router.post('/postreduce', BinanceController.postReduce);
 
+//ARIF
 router.get('/servertimearif', BinanceArifController.getServertime);
 router.get('/balancearif', BinanceArifController.getBalance);
 router.get('/positionarif', BinanceArifController.getPosition);
 router.get('/tradelistarif', BinanceArifController.getTradelist);
 router.post('/posttradearif', BinanceArifController.postTrade);
 router.post('/postreducearif', BinanceArifController.postReduce);
+
+//MA
+router.get('/servertimema', BinanceMAController.getServertime);
+router.get('/balancema', BinanceMAController.getBalance);
+router.get('/positionma', BinanceMAController.getPosition);
+router.get('/tradelistma', BinanceMAController.getTradelist);
+router.post('/posttradema', BinanceMAController.postTrade);
+router.post('/postreducema', BinanceMAController.postReduce);
 
 module.exports = router;
